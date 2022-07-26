2d59caff8d53ff0367d09f91e3977dd55dd9128c
AntiRain888 <110018207+AntiRain888@users.noreply.github.com>
2022-07-26 16:53:34 +0800

    2.2 最终

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,18 +5,11 @@
 int main()
 {
 	using namespace std;
-	cout << "请输入长度"<<endl;
+	cout << "请输入long的值"<<endl;
 	int x,y;
 	cin >> x;
 	y = 220 * x;
 	cout << "结果是" << y << "码";
 	return 0;
 
-
-
-
-	
-
-
-  
 }
