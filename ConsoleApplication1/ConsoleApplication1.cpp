6194fb95abcfb165f14d8b16901c62c78d9f9f32
AntiRain888 <110018207+AntiRain888@users.noreply.github.com>
2022-07-26 16:51:24 +0800

    2.2

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿//2.2解决方案
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	cout << "请输入长度"<<endl;
+	int x,y;
+	cin >> x;
+	y = 220 * x;
+	cout << "结果是" << y << "码";
+	return 0;
+
+
+
+
+	
+
+
+  
+}
